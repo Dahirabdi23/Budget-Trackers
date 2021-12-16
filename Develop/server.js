590cf3cb6697b0c06e17414c552b415e11eb9605
Dahirabdi23 <86687537+Dahirabdi23@users.noreply.github.com>
2021-12-15 21:43:59 -0500

    fixed heroku issue

--- Develop/server.js
+++ Develop/server.js
@@ -3,9 +3,10 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgettracker";
 
 app.use(logger("dev"));
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budgettracker", {
+mongoose.connect( MONGODB_URI , {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
